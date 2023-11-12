dd535b098b7995993ef1b45924b2926dbfadf710
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-12 21:36:09 +0300

    update settings

--- uploader/uploader/settings.py
+++ uploader/uploader/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "files_iploader",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
