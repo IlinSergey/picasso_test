98231cd60ba65dc3d0137672e1a7fa9639790a89
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-12 22:02:41 +0300

    model, serializer, view

--- uploader/files_uploader/views.py
+++ uploader/files_uploader/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import FileSerializer
+
+
+class FileUploadView(APIView):
+    def post(self, request, format=None):
+        file_serializer = FileSerializer
+        if file_serializer.is_valid():
+            file_serializer.save()
+            # Celery
+            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
