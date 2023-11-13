2151d2bdb2e05b0c22de17e62024c6c208c39065
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-13 23:12:39 +0300

    add check file type

--- uploader/files_uploader/services.py
+++ uploader/files_uploader/services.py
@@ -1,16 +1,25 @@
 from asgiref.sync import sync_to_async
+import asyncio
+import mimetypes
 
 import aiofiles
-import asyncio
 
 from files_uploader.models import File
+
+
+def check_file_type(path_to_file: str) -> str | None:
+    '''Проверяем тип файла для последующей, различной обработки(при необходимости)'''
+    mime_type, encoding = mimetypes.guess_type(path_to_file)
+    return mime_type
 
 
 async def process_file(file_id: int) -> None:
     '''Имитируем обработку файла'''
     file_object = await sync_to_async(File.objects.get)(id=file_id)
+    await sync_to_async(check_file_type)(file_object.file.path)
     try:
         async with aiofiles.open(file_object.file.path, mode='rb') as file:
             await asyncio.sleep(2)
     except FileNotFoundError:
         pass
+
