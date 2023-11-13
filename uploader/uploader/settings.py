6cc558b67c6a2ef053df93bf4262850a26f3d2cd
Sergey Ilin <ilin_sergey90@mail.ru>
2023-11-13 20:49:12 +0300

    config file

--- uploader/uploader/settings.py
+++ uploader/uploader/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from ..config import SECRET_KEY
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ciwe%26t#d&v)bww2@bu^#t_dpb^_9a@+emc$1(^+^e2izbx%@"
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
